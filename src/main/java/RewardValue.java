a9544b1994094fa47df933cf1af32dbc69b83cac
kodakandlasindhu <kodakvyshu@gmail.com>
2024-07-29 12:24:38 -0500

    Modified test cases

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,21 +1,28 @@
 public class RewardValue {
-    private double cashValue;
-    private double milesValue;
-    private static final double M2C_Conversion_Rate =0.0035;
-    public RewardValue(double cashValue){
-        this.cashValue=cashValue;
-        this.milesValue=cashValue/M2C_Conversion_Rate;
-    }
-    public RewardValue(int milesValue){
-        this.milesValue=milesValue;
-        this.cashValue=cashValue/M2C_Conversion_Rate;
+    private final double cashValue;
+    public static final double M2C_Conversion_Rate = 0.0035;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
     }
 
-    public double getCashValue(){
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
+    }
+
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / M2C_Conversion_Rate);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * M2C_Conversion_Rate;
+    }
+
+    public double getCashValue() {
         return cashValue;
     }
-    public int getMilesValue(){
-        return (int) milesValue;
-    }
 
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
+    }
 }
